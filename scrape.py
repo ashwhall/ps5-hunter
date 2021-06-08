07cf07c453e5375ceb3d16f947171d66f5d83f0b
Ash <ashwhall@gmail.com>
2021-06-09 08:54:17 +1000

    Fix broken BIG W scraping; tidying

--- scrape.py
+++ scrape.py
@@ -4,14 +4,10 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 
 
-def scrape_bigw():
-    print('Scraping BIG W... ', end='', flush=True)
-    has_stock = True
-    resp = requests.get('https://www.bigw.com.au/entertainment/video-games-consoles/ps5/ps5-consoles')
+def scrape_site(name, scrape_func):
+    print(f'Scraping {name}... ', end='', flush=True)
 
-    if resp.status_code == 404 or 'That page has either been moved' in resp.text:
-        has_stock = False
-
+    has_stock = scrape_func()
     if has_stock:
         print('Stock found :D')
     else:
@@ -20,10 +16,19 @@ def scrape_bigw():
     return has_stock
 
 
-def selenium_get_with_wait(driver, lambdaa, timeout=3):
+def scrape_bigw():
+    resp = requests.get('https://www.bigw.com.au/ps5')
+
+    if resp.status_code == 404 or 'Please continue to check back online for further updates' in resp.text:
+        return False
+
+    return True
+
+
+def selenium_get_with_wait(driver, lambd, timeout=3):
     elements = []
     try:
-        elements = WebDriverWait(driver, timeout=timeout).until(lambdaa)
+        elements = WebDriverWait(driver, timeout=timeout).until(lambd)
     except selenium.common.exceptions.TimeoutException:
         pass
     return elements
@@ -34,42 +39,25 @@ def selenium_element_contains_string(driver, string):
 
 
 def scrape_target(driver):
-    print('Scraping Target... ', end='', flush=True)
-
     driver.get('https://www.target.com.au/playstation-5')
 
     no_stock_str = 'Releasing soon'
     not_available_text = selenium_element_contains_string(driver, no_stock_str)
-    has_stock = True
     if not_available_text or isinstance(not_available_text, list):
-        has_stock = False
+        return False
 
-    if has_stock:
-        print('Stock found :D')
-    else:
-        print('No stock found :(')
-
-    return has_stock
+    return True
 
 
 def scrape_jb(driver):
-    print('Scraping JB HI-FI... ', end='', flush=True)
-
     driver.get('https://www.jbhifi.com.au/collections/games-consoles/playstation-consoles')
 
     ps5_strings = ('ps5', 'playstation 5', ' 5 ')
-    has_stock = False
     for title in selenium_get_with_wait(driver, lambda d: d.find_elements_by_class_name('product-tile__title')):
         if any(s in title.get_attribute('innerText').lower() for s in ps5_strings):
-            has_stock = True
-            break
+            return True
 
-    if has_stock:
-        print('Stock found :D')
-    else:
-        print('No stock found :(')
-
-    return has_stock
+    return False
 
 
 bigw_found = target_found = jb_found = False
@@ -85,13 +73,13 @@ def scrape(headless=False):
     driver = webdriver.Chrome(executable_path='chromedriver_linux64/chromedriver', options=chrome_options)
 
     stock_str = ''
-    if not bigw_found and scrape_bigw():
+    if not bigw_found and scrape_site('BIG W', scrape_bigw):
         bigw_found = True
-        stock_str += 'BIG W appears to have stock! https://www.bigw.com.au/entertainment/video-games-consoles/ps5/ps5-consoles\n'
-    if not target_found and scrape_target(driver):
+        stock_str += 'BIG W appears to have stock! https://www.bigw.com.au/ps5\n'
+    if not target_found and scrape_site('Target', lambda: scrape_target(driver)):
         target_found = True
         stock_str += 'Target appears to have stock! https://www.target.com.au/playstation-5\n'
-    if not jb_found and scrape_jb(driver):
+    if not jb_found and scrape_site('JB HI-FI', lambda: scrape_jb(driver)):
         jb_found = True
         stock_str += 'JB HIFI appears to have stock! https://www.jbhifi.com.au/collections/games-consoles/playstation-consoles\n'
 
